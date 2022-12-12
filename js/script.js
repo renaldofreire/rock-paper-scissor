d7137d05bc9ef232c5c4c990d6a1636c626a6bd4
Renaldo Freire <renaldo@disroot.org>
2022-12-12 07:09:35 -0300

    Complete project up to topic 5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-function getComputerChoice() {
-  const options = ['rock', 'paper', 'scissors']
+const options = ['rock', 'paper', 'scissors']
 
+function getComputerChoice() {
   let randomChoice = options[Math.floor(Math.random() * 3)]
-  return console.log(randomChoice)
+  return randomChoice
 }
 
-getComputerChoice()
+function playGame(playerSelection, computerSelection) {
+
+  if (playerSelection === computerSelection) {
+    return console.log(`Os dois valores são iguais: ${playerSelection}`)
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return console.log('VENCEU! Player escolhe rock. IA escolhe scissors.')
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return console.log('PERDEU! Vc escolheu rock, a IA escolheu paper!')
+  } else if (playerSelection !== 'rock') {
+    return console.log('Insira um valor válido.')
+  }
+}
+
+const playerSelection = 'rOcK'.toLowerCase()
+const computerSelection = getComputerChoice()
+console.log(computerSelection)
+
+playGame(playerSelection, computerSelection)
+
+// NOTE completo até tópico 4
+
