841d5ef7834bcc02ba0634a56d645978c86c338c
Renaldo Freire <renaldo@disroot.org>
2022-12-18 08:09:04 -0300

    Functional game function using do while

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const options = ['rock', 'paper', 'scissors']
 let randomChoice
 let playerSelection
 let computerSelection
-let computerScore
-let playerScore 
+let computerScore = 0
+let playerScore = 0
 
 function getComputerChoice() {
   randomChoice = options[Math.floor(Math.random() * 3)]
@@ -19,23 +19,36 @@ function playRound(playerSelection, computerSelection) {
     let txt = `Os dois valores são os mesmos: ${playerSelection}`
     return txt
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore++
     let txt = 'VENCEU! Player escolhe rock. IA escolhe scissors.'
     return txt
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     let txt = 'PERDEU! Vc escolheu rock, a IA escolheu paper!'
+    computerScore++
     return txt
   } else if (playerSelection !== 'rock') {
-    let txt = 'Insira um valor válido.'
+    let txt = 'Player: insira um valor válido "rock"'
     return txt
   }
 }
 
-console.log(playRound(playerSelection, computerSelection))
-
 // console.log(playRound(playerSelection, computerSelection))
 
-// function game() {
-//   for (i = 0; i < 5; i++) {
-//     playRound(playerSelection, computerSelection)
-//   }
-// }
+function game() {
+  do {
+    playRound(playerSelection, computerSelection)
+    console.log(`AI tem ${computerScore}`)
+    console.log(`Player tem ${playerScore}\n`)
+  } while (playerScore <= 4 && computerScore <= 4)
+  {
+    if (playerScore === 5) {
+      let txt = `Usuário venceu, por ${playerScore} pontos. AI fez ${computerScore} pontos.`
+      return console.log(txt)
+    } else {
+      let txt = `AI venceu, por ${computerScore} pontos. Usuário fez ${playerScore} pontos.`
+      return console.log(txt)
+    }
+  }
+}
+
+game()
